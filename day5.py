3fa4303e0b023852b0e2f9f93ca7ac9bef4992f2
nibedita21-ai <nibeditaswain351@gmail.com>
2025-12-27 18:12:14 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,14 @@
+# Write a python program to print the contents of a dictionary using the os module.
+
+
+# Define a sample dictionary
+my_dictionary = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+    "occupations": ["Engineer", "Writer"]
+}
+
+# Print the entire dictionary
+print("Printing the entire dictionary:")
+print(my_dictionary)
