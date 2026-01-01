2c1af282c76fafd8d7d13b686ee655e6cf5d8234
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-01 18:29:47 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,64 @@
+name = "Nibedita"
+age = 22
+
+print(name,age)
+
+print("My Name is :" , name)
+print("My Age is:" , age)
+
+a = 3
+b = 2
+print(a+b)
+
+
+a = 6
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+a = 20
+b = 30
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+
+num = 10
+# num = num + 10  #(num = num + 10) can also be written as (num += 10) both have same meaning.
+num += 10 #Assignment operators (+=)
+print("num :", num)
+
+
+#Logical operators
+print(not False)
+print(not True)
+
+a = 50
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("and operator : ", val1 and val2)
+print("or operator : ", val1 or val2)
+
+val1 = True
+val2 = True
+print("and operator : ", val1 and val2)
+print("or operator : ", val1 or val2)
+
+val1 = False
+val2 = False
+print("and operator : ", val1 and val2)
+print("or operator : ", val1 or val2)
+
+
