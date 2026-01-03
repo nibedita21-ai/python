8bc938c1d9837f20584375e2ef00de863f21b56f
nibedita21-ai <nibeditaswain351@gmail.com>
2026-01-03 22:34:05 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,5 @@
+# write a program to input 2 numbers & print their sum.
+first = int(input("Enter First Number : "))
+second = int(input("Enter Second Number : "))
+sum = first + second
+print("Sum = ", sum)
